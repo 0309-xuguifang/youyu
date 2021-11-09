8339c4d1713a84f6788ae26c47f334f38ad5c9d5
0309-xuguifang <x07191822@163.com>
2021-11-09 17:13:46 +0800

    firt commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,5 +9,29 @@ Page({
     .catch(err=>{
       console.log('云函数获取数据失败',err)
     })
-  }
+  },
+  data: {
+    active: 0,
+    icon1: {
+      normal: '../../images/detail.png',
+      active: '../../images/detail2.png',
+    },
+    icon2:{
+      normal: '../../images/statistics.png',
+      active: '../../images/statistics2.png',
+    },
+    icon3:{
+      normal: '../../images/me.png',
+      active: '../../images/me2.png',
+    }
+  },
+  onChange(event) {
+    this.setData({ active: event.detail });
+  },
+  onClick(event) {
+    wx.showToast({
+      title: '点击标签 ${event.detail + 1}',
+      icon: 'none',
+    });
+  },
 })
